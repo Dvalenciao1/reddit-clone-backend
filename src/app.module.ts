d400a0c7f654c7e783ea67dafecab2ea93b726ff
Dvalenciao1 <dv237968@gmail.com>
2024-09-03 20:01:08 -0500

    config eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
 import { InteractionModule } from './interaction/interaction.module';
 import { PostModule } from './post/post.module';
