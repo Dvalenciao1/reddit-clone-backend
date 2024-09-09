f25c1631a151d587edf8123c66dc118cb3942f5b
Dvalenciao1 <dv237968@gmail.com>
2024-09-09 01:03:13 +0000

    create user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  pasword: string;
+
+  @Column({ type: 'boolean', nullable: false, default: false })
+  deleted: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
