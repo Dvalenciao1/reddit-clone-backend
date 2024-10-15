b62556e06b8a195b2e094e78dbd7b4023c62b54a
Daniel Valencia <dv237968@gmail.com>
2024-10-15 11:08:43 -0500

    add auth controller and srvice with dtos

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateAuthDto } from './create-auth.dto';
+import { CreateAuthDto } from './signup.dto';
 
 export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
