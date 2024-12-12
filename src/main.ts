2554473770215a182e98c5f3945167b4c361265b
Dvalenciao1 <dv237968@gmail.com>
2024-12-11 19:54:16 -0500

    feat(app): add pipes validations and filters for handling errors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HTTPExceptionFilter } from './common/filters/HttpExceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new HTTPExceptionFilter());
   app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
